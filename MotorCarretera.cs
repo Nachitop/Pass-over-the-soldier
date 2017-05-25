3f914d41ee691647e6f2a434a2dde21fd64a6ada
EduardoCota <eduardocc.8.20@gmail.com>
2017-05-25 15:40:00 -0600

    Add files via upload

---
+++ MotorCarretera.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorCarretera : MonoBehaviour {
+    public float velocidad;
+    public bool inicio;
+    public bool final;
+    public bool pausa;
+
+    public GameObject ContenedorCalleGo;
+    public GameObject[] ContenedorCalleArray;
+
+    int contadorCalle = 0;
+    int numeroSelectorCalle;
+
+    public GameObject calleAnterior;
+    public GameObject calleNuevo;
+
+    public Vector3 medidaLimitePantalla;
+    public bool salidaPantalla;
+    public GameObject mCamGo;
+    public Camera mCamComp;
+
+    public float calleSize;
+
+
+    public GameObject tankGO;
+    public GameObject bgFinalGO;
+
+
+
+    // Use this for initialization
+    void Start () {
+        InicioJuego();
+	}
+    void InicioJuego()
+    {
+        ContenedorCalleGo = GameObject.Find("ContenedorCalles");
+        mCamGo  = GameObject.Find("Main Camera");
+        mCamComp=mCamGo.GetComponent< Camera >();
+        bgFinalGO = GameObject.Find("Panel");
+        bgFinalGO.SetActive(false);
+
+        tankGO = GameObject.FindObjectOfType<Tank>().gameObject;
+
+        VelocidadMotor();
+        BuscarCalle();
+
+
+    }
+    void VelocidadMotor()
+    {
+        velocidad = 8;
+    }
+    void BuscarCalle()
+    {
+        ContenedorCalleArray = GameObject.FindGameObjectsWithTag("calle");
+        for (int i = 0; i < ContenedorCalleArray.Length; i++)
+        {
+            ContenedorCalleArray[i].gameObject.transform.parent = ContenedorCalleGo.transform;
+            ContenedorCalleArray[i].gameObject.SetActive(false);
+            ContenedorCalleArray[i].gameObject.name = "CalleOFF_" + i;
+        }
+        CrearCalle();
+    }
+    void CrearCalle()
+    {
+        contadorCalle++;
+        numeroSelectorCalle = Random.Range(0, ContenedorCalleArray.Length);
+        GameObject Calle = Instantiate(ContenedorCalleArray[numeroSelectorCalle]);
+        Calle.SetActive(true);
+        Calle.name = "Calle" + contadorCalle;
+        Calle.transform.parent = gameObject.transform;
+        PosicionarCalle();
+    }
+    void PosicionarCalle()
+    {
+        calleAnterior = GameObject.Find("Calle" + (contadorCalle - 1));
+        calleNuevo = GameObject.Find("Calle" + contadorCalle);
+        MedirCalle();
+        calleNuevo.transform.position = new Vector3(calleAnterior.transform.position.x, calleAnterior.transform.position.y + calleSize-2F, 0);
+        salidaPantalla = false;
+    }
+
+    void MedirCalle()
+    {
+        for (int i = 0; i < calleAnterior.transform.childCount; i++)
+        {
+            if (calleAnterior.transform.GetChild(i).gameObject.GetComponent<Pieza>() != null) { }
+            float piezaSize = calleAnterior.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().bounds.size.y;
+            calleSize = calleSize + piezaSize;
+        }
+    }
+
+    void MedirPantalla()
+    {
+        medidaLimitePantalla = new Vector3(0, mCamComp.ScreenToWorldPoint(new Vector3(0, 0, 0)).y - 0.5f, 0);
+    }
+    void DestruyoCalles()
+    {
+        Destroy(calleAnterior);
+        calleSize = 0;
+        calleAnterior = null;
+        CrearCalle();
+    }
+    // Update is called once per frame
+
+
+         public void JuegoEstados()
+    {
+        bgFinalGO.SetActive(true);
+    }
+    void Update () {
+       if (inicio == true && final == false)
+        {
+            transform.Translate(Vector3.down * velocidad * Time.deltaTime);
+            if(calleAnterior.transform.position.y + calleSize <medidaLimitePantalla.y && salidaPantalla==false)
+            {
+                salidaPantalla = true;
+                DestruyoCalles();
+                
+              
+            }
+        }
+
+    }
+}
---
+++ soldado.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soldado : MonoBehaviour {
+
+    public GameObject cronometroGO;
+    public Cronometro cronometroScript;
+    public GameObject motorCarreteraGO;
+    public MotorCarretera motorCarreteraScript;
+
+    private void Start()
+    {
+        cronometroGO = GameObject.FindObjectOfType<Cronometro>().gameObject;
+        cronometroScript = cronometroGO .GetComponent<Cronometro>();
+        motorCarreteraGO = GameObject.Find("MotorCarretera");
+        motorCarreteraScript = motorCarreteraGO.GetComponent<MotorCarretera>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Tank>() != null)
+        {
+            cronometroScript.score = cronometroScript.score + 3;
+            motorCarreteraScript.velocidad = motorCarreteraScript.velocidad + 1;
+            Destroy(this.gameObject);
+            
+        }
+    }
+}
